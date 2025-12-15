fef91ef254a142df715ff51a78b2915a99a0cf0b
matteovecchio04 <matteovex@gmail.com>
2025-12-15 18:11:44 +0100

    add posts router and connect to post controller

---
+++ routers/posts.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const postsController = require("../controllers/postController")
+
+const router = express.Router()
+
+router.get("/", postsController.index)
+
+router.get("/:id", postsController.show)
