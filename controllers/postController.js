d81512fcd72c59adae33a52de172509d9c6dee91
matteovecchio04 <matteovex@gmail.com>
2025-12-15 18:43:00 +0100

    add show function

--- controllers/postController.js
+++ controllers/postController.js
@@ -11,7 +11,30 @@ const index = (req, res) => {
     })
 }
 
+const show = (req, res) => {
+    // prevent admin rights
+    const id = Number(req.params.id)
+    const query = `SELECT * FROM posts WHERE id = ?`
+    const queryTags = `SELECT tags.id, tags.label FROM tags JOIN post_tag ON post_tag.tag_id = tags.id WHERE post_tag.post_id = ?`
+
+    connection.query(query, [id], (err, response) => {
+        if (err) return res.status(500).json({ error: err, message: err.message });
+
+        if (response.length === 0)
+            return res.status(404).json({ error: 404, message: "Post not found" });
+
+        connection.query(queryTags, [id], (errTags, resTags) => {
+            if (errTags)
+                return res
+                    .status(500)
+                    .json({ error: errTags, message: errTags.message });
+
+            res.json({ ...response[0], tags: resTags });
+        });
+    });
+}
 
 module.exports = {
     index
+    show
 }
